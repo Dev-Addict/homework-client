383134f7f820063341357c2ccf2429f55a373460
WolfMAN <aria.azadi.pour@protonmail.com>
2020-05-13 22:41:46 +0430

    add style to AdminsButton

--- src/components/AdminButtons.js
+++ src/components/AdminButtons.js
@@ -2,11 +2,13 @@ import React from "react";
 import {connect} from 'react-redux';
 import {Link} from "react-router-dom";
 
+import '../style/components/AdminButtons.css';
+
 const AdminButtons = props => {
     return (
         <div>
             <Link to="/create">
-                <button>create User</button>
+                <button className="admin-buttons-button">create User</button>
             </Link>
         </div>
     );
