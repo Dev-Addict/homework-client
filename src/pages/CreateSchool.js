dde3ff21a027caa0910bba23282fb9faf5266c1f
WolfMAN <aria.azadi.pour@protonmail.com>
2020-05-15 00:05:46 +0430

    create school component is ready

--- src/pages/CreateSchool.js
+++ src/pages/CreateSchool.js
@@ -2,13 +2,38 @@ import React from "react";
 import {connect} from "react-redux";
 
 import SchoolForm, {formFields} from "../components/SchoolForm";
+import {createSchool} from "../actions";
+import history from "../history";
+import UserForm from "../components/UserForm";
 
 const CreateSchool = props => {
+    if (!props.auth.isSigned) {
+        history.push('/');
+        return (<div/>);
+    }
+
+    if (['teacher', 'student'].includes(props.auth.data.user.rote)) {
+        history.push('/dashboard');
+        return (<div/>);
+    }
+
+    const onSubmit = formValues => {
+        props.createSchool({
+            name: formValues[formFields.name] || undefined,
+            manager: formValues[formFields.manager] || undefined
+        });
+    };
+
     return (
-        <div>
-            CreateSchool
-        </div>
+        <SchoolForm onSubmit={(onSubmit)} rote={props.auth.data.user.rote} initialValues={{}} err={props.err}/>
     );
 };
 
-export default connect()(CreateSchool);
+const mapStateToProps = state => {
+    return {
+        auth: state.auth,
+        err: state.err
+    };
+};
+
+export default connect(mapStateToProps, {createSchool})(CreateSchool);
