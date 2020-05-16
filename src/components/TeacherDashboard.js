bde6978c1da196a1f10272597fba7a47df5954dc
WolfMAN <aria.azadi.pour@protonmail.com>
2020-05-16 21:24:41 +0430

    write teacher Dashboard

--- src/components/TeacherDashboard.js
+++ src/components/TeacherDashboard.js
@@ -1,11 +1,51 @@
-import React from "react";
+import React, {useLayoutEffect} from "react";
+import {connect} from 'react-redux';
+
+import {getClasses, getLessons} from "../actions";
+import '../style/components/TeacherDashboard.css';
 
 const TeacherDashboard = props => {
+    useLayoutEffect(() => {
+        props.getClasses();
+        props.getLessons();
+    }, []);
+
+    const renderTeacherCards = () => props.classes.map((classData, index) => (
+        <div className="teacher-dashboard-teacher-card">
+            <div className="teacher-dashboard-teacher-card-class">Class: {classData.name}</div>
+            <div className="teacher-dashboard-teacher-card-lesson">lesson: {props.lessons[index].name}</div>
+        </div>
+    ));
+
     return (
-        <div>
-            TeacherDashboard
+        <div className="teacher-dashboard-container">
+            {renderTeacherCards()}
         </div>
     );
 };
 
-export default TeacherDashboard;
+const mapStateToProps = (state) => {
+    const classes = [];
+    const lessons = [];
+    state.classes.forEach(classData => classData.lessons.forEach(lesson => {
+        let lessonData;
+
+        state.lessons.forEach(lessonD => {
+            if (lessonD._id === lesson) {
+                lessonData = lessonD;
+            }
+        });
+
+        if ((lessonData || {}).teacher === state.auth.data.user._id) {
+            classes.push(classData);
+            lessons.push(lessonData);
+        }
+    }));
+
+    return {
+        classes,
+        lessons
+    };
+};
+
+export default connect(mapStateToProps, {getClasses, getLessons})(TeacherDashboard);
